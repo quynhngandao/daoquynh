929ad2c8aeac61b5665cf13c01d0fc2743f71dba
quynhngandao <quynhngannguyendao@gmail.com>
2026-05-15 12:06:53 -0500

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://daoquynh.github.io',
+  site: 'https://quynhngandao.github.io',
   integrations: [tailwind({ applyBaseStyles: false })],
   output: 'static',
 });
